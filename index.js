0e8809ec6793acfc87d198db763fa29168f59ead
BabulKumar2022 <bb3583400@gmail.com>
2022-05-13 19:03:04 +0600

    mongo connected and geting data

--- index.js
+++ index.js
@@ -10,7 +10,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://db_doctor:<password>@cluster0.guz4u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.guz4u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run(){
+    try{
+        await client.connect();
+        const treatmentCollection = client.db('doctor_service').collection('treatment');
+        console.log('db connected');
+        app.get('/service', async(req, res) =>{
+            const query ={};
+            const cursor = treatmentCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello Doctor on UI')
